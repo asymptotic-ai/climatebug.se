2e92185cc038677ada8e4d8af5d04d208dd4b393
Samuel <samuel@scheidegger.se>
2023-10-22 13:40:42 +0200

    fix bugs

--- docs/views/Home.vue
+++ docs/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div>
-    <Menu />
-    <section class="text-center py-20">
-      <!-- Hero & Other Content Here, styled with our pastel theme -->
-    </section>
-  </div>
+    <div>
+        <Menu />
+        <section class="text-center py-20">
+            <!-- Hero & Other Content Here, styled with our pastel theme -->
+            Hello
+        </section>
+    </div>
 </template>
 
 <script>
 import Menu from '../components/Menu.vue';
 
 export default {
-  components: {
-    Menu
-  }
+    components: {
+        Menu
+    }
 };
 </script>
